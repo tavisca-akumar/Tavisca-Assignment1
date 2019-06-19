dfcee7464a1c60fecad044d230e96472d34d6be7
Kumar Ankit <akumar.tavisca.in>
2019-06-19 20:01:11 +0530

    Adding two values

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,14 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		int c=a+b;
+		System.out.println("c");
+	
 		System.out.println("hello world");
+		System.out.println("Change in file");
+
 
 	}
 
