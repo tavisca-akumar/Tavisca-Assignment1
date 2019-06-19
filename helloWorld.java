9fb8b33d926880b778675643dbc5dee3074a9316
Kumar Ankit <akumar.tavisca.in>
2019-06-19 23:59:00 +0530

    Hello uses Greeter

---
+++ helloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		int c=a+b;
+		System.out.println("c");
+	
+		// Author: Kumar Ankit(ankit221505@gmail.com)
+
+
+		System.out.println("hello world");
+		System.out.println("Change in file");
+
+		Greeter g=new Greeter();
+		g.
+
+	}
+
+}
